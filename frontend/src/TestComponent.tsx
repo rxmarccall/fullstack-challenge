0d78ac28df30c039130d451db3b4901f4c48baa2
Marc <rxmarccall@gmail.com>
2025-01-11 11:00:46 -0700

    - Added Accounts and Deals Tables. Simple TestComponent to begin to work on front end fetching from backend.

---
+++ frontend/src/TestComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestComponent: React.FC = () => {
+  return (
+    <div>
+      HEY LISTEN
+    </div>
+  );
+}
+
+export default TestComponent;
--- server/db.ts
+++ server/db.ts
@@ -2,6 +2,8 @@ import Database from "better-sqlite3";
 
 function initializeDatabase() {
   const db = new Database("./database.sqlite", { verbose: console.log });
+  
+  // Organizations Table
   db.prepare(
     `
     CREATE TABLE IF NOT EXISTS organizations (
@@ -12,7 +14,33 @@ function initializeDatabase() {
     );
   `
   ).run();
-  // TODO: Add your account and deal tables schemas here
+
+  // Accounts Table
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS accounts (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      organization_id INTEGER NOT NULL,
+      name TEXT NOT NULL,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    `
+  ).run();
+
+  // Deals Table
+  db.prepare(
+    `
+    CREATE TABLE IF NOT EXISTS deals (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      account_id INTEGER NOT NULL,
+      name TEXT NOT NULL,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    `
+  ).run();
+
   return db;
 }
 
