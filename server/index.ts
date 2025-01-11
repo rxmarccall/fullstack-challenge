7bef2cf20a2b08a68689b1f0b10d6c65225e51da
Marc <rxmarccall@gmail.com>
2025-01-11 12:19:38 -0700

    Created endpoint to get all deals for a given organization_id

--- server/index.ts
+++ server/index.ts
@@ -30,3 +30,31 @@ app.get("/api/organizations", (req, res) => {
   const rows = db.prepare("SELECT * FROM organizations").all();
   res.json(rows);
 });
+
+app.get("/api/deals/:organization_id", (req, res) => {
+    const organizationId = req.params.organization_id;
+
+    const selectStatement = `
+    SELECT 
+        deals.id AS deal_id,
+        deals.name AS deal_name,
+        deals.created_at AS deal_created_at,
+        deals.updated_at AS deal_updated_at,
+        accounts.id AS account_id,
+        accounts.name AS account_name,
+        accounts.created_at AS account_created_at,
+        accounts.updated_at AS account_updated_at
+    FROM 
+        deals
+    JOIN 
+        accounts ON deals.account_id = accounts.id
+    JOIN 
+        organizations ON accounts.organization_id = organizations.id
+    WHERE 
+        organizations.id = ?;
+  `;
+
+
+    const rows = db.prepare(selectStatement).all(organizationId);
+    res.json(rows);
+});
